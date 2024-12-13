396261ec74f77d878d16a959bc801f819d9965f4
victorroddev <javier.rod.dev@gmail.com>
2024-12-13 12:10:44 -0700

    Tailwind Config file setup ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundWhite: 'rgba(255, 255, 255)',
+        backgroundGreen: 'rgba(12, 166, 128)',
+        blue: 'rgba(94, 174, 235)'
+      },
+      fontFamily :{
+        headers: ['Poppins'],
+        paragraphs: ['Noto Sans']
+      }
+    },
   },
   plugins: [],
 }
